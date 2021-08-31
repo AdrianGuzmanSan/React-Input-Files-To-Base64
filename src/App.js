408be4a45f52120cd11e19c59946a2cc560e0c36
ADRIAN GUZMAN SANABRIA <al194466@alumnos.uacj.mx>
2021-08-31 14:33:22 -0600

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+
+  const convertirABase64 = (archivo) => {
+  // const convertirABase64 = (archivos) => {
+    // Array.from(archivos).forEach(archivo =>{
+      var reader = new FileReader();
+      // Archivo debe estar en el índice [0] para que no aparezca el error: 
+      // TypeError: FileReader.readAsDataURL: Argument 1 does not implement interface Blob.
+      // files property is a FileList (an array like structure), 
+      // you need to access and pass the individual file objects in that list, eg readAsDataURL(files[0])
+      reader.readAsDataURL(archivo[0]);
+      reader.onload=function(){
+        var arrayAuxiliar=[]; 
+        var base64 = reader.result;
+        console.log(base64);
+        arrayAuxiliar=base64.split(',');
+        console.log(arrayAuxiliar[1]);
+      }
+    // })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <input type='file' onChange={(e) => convertirABase64(e.target.files)}/>
     </div>
   );
 }
